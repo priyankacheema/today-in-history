e0a74319abe83cf1013c3be93541f28d15aed56c
Priyanka Cheema <pcheema@qca-sdc.com>
2018-09-25 09:37:10 -0500

    chore: enhance UI

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import { createForm } from "./createForm";
 import * as header from "./createHeader";
 import { historyCollection } from "./todayInHistory";
 import css from "./style.css";
+import icon from "./history.jpg";
 
 header.createHeader("h1", "Today in History");
 const histForm = createForm({ id: "history-form" });
+
+// Add the image to our existing div.
+// const div = document.createElement("div");
+// const myIcon = new Image();
+// myIcon.src = icon;
+
+// element.appendChild(myIcon);
 
 const todayInHistoryCollection = () => {
   histForm.addEventListener("submit", e => {
